c93b3817b33a81e7353365c2b675f08c76f7d398
StudioChew <octopusthatisgreat@gmail.com>
2020-05-31 19:31:12 +0300

    Create funstuff.py

---
+++ funstuff.py
@@ -0,0 +1,15 @@
+# fun stuff
+# fun cool epic
+
+print("wel;ocme to fun")
+print("choose a, b, or c")
+epic = input("chooser 3000>")
+if epic == "a":
+    print("i like fire trucls and moster tucks")
+    exit()
+elif epic == "b":
+    print("i like blaze and the monster machines i want to be truck driver in future cool")
+    exit()
+elif epic == "c":
+    print("print('walter')")
+    exit()
